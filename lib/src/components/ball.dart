31bef54565df67b413d21f46b0a26eff80d7a819
Khubaib Jamal <khubaib.j18@gmail.com>
2025-08-02 21:22:57 +0500

    feat:bouncing effect on game

--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -1,7 +1,11 @@
+import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:brick_breaker/src/components/play_area.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 
-class Ball extends CircleComponent {
+class Ball extends CircleComponent
+    with CollisionCallbacks, HasGameReference<BrickBreaker> {
   Ball({
     required this.velocity,
     required super.position,
@@ -9,9 +13,11 @@ class Ball extends CircleComponent {
   }) : super(
          radius: radius,
          anchor: Anchor.center,
-         paint: Paint()
-           ..color = const Color(0xff1e6091)
-           ..style = PaintingStyle.fill,
+         paint:
+             Paint()
+               ..color = const Color(0xff1e6091)
+               ..style = PaintingStyle.fill,
+         children: [CircleHitbox()],
        );
 
   final Vector2 velocity;
@@ -20,5 +26,26 @@ class Ball extends CircleComponent {
   void update(double dt) {
     super.update(dt);
     position += velocity * dt;
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is PlayArea) {
+      if (intersectionPoints.first.y <= 0) {
+        velocity.y = -velocity.y;
+      } else if (intersectionPoints.first.x <= 0) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.x >= game.width) {
+        velocity.x = -velocity.x;
+      } else if (intersectionPoints.first.y >= game.height) {
+        removeFromParent();
+      }
+    } else {
+      debugPrint('collision with $other');
+    }
   }
 }
